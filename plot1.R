94c58f1d53b0a078829ce5d64845c7ca1761d39b
CiccioIdoine <ciccio@gmail.com>
2014-06-22 21:11:49 +0200

    Correction to the plot1 R and ONG files

--- plot1.R
+++ plot1.R
@@ -2,14 +2,14 @@
 NEI <- readRDS("../summarySCC_PM25.rds")
 SCC <- readRDS("../Source_Classification_Code.rds")
 
-# Sum emissions per year and divide for readability
+# Sum emissions per year
 sums <- aggregate(NEI$Emissions, by=list(Year = NEI$year), FUN=sum)
 
 # Create PNG file
 png(file = "plot1.png", width = 480, height = 480)
 
 # Plot the data
-plot(sums[,1], main="Total emissions from PM2.5 in the United States (1998 to 2008)", ylab="Emissions (in 1000s)")
+plot(sums[,1], sums[,2], type="l", main="Total emissions from PM2.5 in the United States (1998 to 2008)", ylab="Emissions (in 1000s)")
 
 # Close dev
 dev.off()
